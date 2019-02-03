d341d9c4fbb678cac85168130dce2ebd373d8416
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-03 11:27:04 -0500

    Added put request for equipment database

--- api/server.js
+++ api/server.js
@@ -93,15 +93,47 @@ server.post('/boardLog', async (req, res) => {
   }
 });
 
-// router.post('/', async (req, res) => {
+server.post('/schoolLog', async (req, res) => {
+  try {
+    const ids = await db('schoolLog').insert(req.body);
+    console.log('req body =', req.body);
+    res.status(201).json(`Added new log with is ${ids}`);
+  } catch (error) {
+    console.log('ERROR', error);
+  }
+});
+
+server.put('/equipment/:id', async (req, res) => {
+  try {
+    const changes = req.body;
+    const myUpdate = await db('equipment')
+      .where({ id: req.params.id })
+      .update(changes);
+    console.log('Changes', changes);
+    console.log('my update', myUpdate);
+    res.status(200).json(myUpdate);
+  } catch (error) {
+    console.log('ERROR', error);
+  }
+});
+
+// router.put('/:id', async (req, res) => {
 //   try {
-//     const ids = await db('project').insert(req.body);
-//     const projectResponse = await db('project').where({ id: ids[0] });
-//     res.status(responseStatus.postCreated).json(projectResponse);
+//     const changes = req.body;
+//     const myUpdate = await db('project')
+//       .where({ id: req.params.id })
+//       .update(changes);
+//     if (!myUpdate) {
+//       res
+//         .status(responseStatus.badRequest)
+//         .json({ message: 'This ID does not exist in the database.' });
+//     } else {
+//       res.status(responseStatus.success).json(myUpdate);
+//     }
 //   } catch (error) {
 //     res
-//       .status(responseStatus.serverError)
-//       .json({ message: 'Error adding project.' });
+//       .status(responseStatus.badRequest)
+//       .json({ errorMessage: 'Unable to update that project.' });
 //   }
 // });
 
